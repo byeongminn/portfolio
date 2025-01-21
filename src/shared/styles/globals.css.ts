eb4d8babce51bb220f266a554dcf8b83f14d7312
byeongminn <sekn1997@naver.com>
2025-01-21 21:00:09 +0900

    feat: FSD 및 vanilla-extract 세팅

---
+++ src/shared/styles/globals.css.ts
@@ -0,0 +1,13 @@
+import { globalStyle } from "@vanilla-extract/css";
+
+globalStyle("*", {
+  boxSizing: "border-box",
+  margin: 0,
+  padding: 0,
+});
+
+globalStyle("html, body", {
+  maxWidth: "100vw",
+  minHeight: "100vh",
+  backgroundColor: "#FAFAFF",
+});
