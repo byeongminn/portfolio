b467528ac3508052bc05355896123f1f81dc9ee1
byeongminn <sekn1997@naver.com>
2025-01-23 17:58:34 +0900

    feat: Projects 데이터 조회 api 생성

---
+++ src/features/projects/api/getProjects.ts
@@ -0,0 +1,24 @@
+import { API_BASE_URL } from "@/shared/constants";
+
+type Project = {
+  title: string;
+  description: string;
+  period: string;
+  achievements: {
+    title: string;
+    contents: string[];
+  }[];
+  stacks: string[];
+};
+
+export type GetProjectsResponse = {
+  projects: Project[];
+};
+
+export const getProjectsURL = `${API_BASE_URL}/api/projects`;
+
+export const getProjects = async (): Promise<GetProjectsResponse> => {
+  const response = await fetch(getProjectsURL);
+
+  return response.json();
+};
