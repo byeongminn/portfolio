ce11b33ec0c27c81057d97451a8e1e292ad7e029
byeongminn <sekn1997@naver.com>
2025-02-28 12:32:28 +0900

    design: About 컴포넌트 반응형 디자인 구현

--- src/features/about/components/About/style.css.ts
+++ src/features/about/components/About/style.css.ts
@@ -14,8 +14,8 @@ export const container = style([
   f.directionColumn,
   {
     margin: "0 auto",
-    padding: "5rem 0",
-    gap: "3.125rem",
+    padding: "2rem 1rem",
+    gap: "1.25rem",
   },
   responsiveStyle({
     xxl: {
@@ -25,13 +25,12 @@ export const container = style([
       maxWidth: WIDTH_XL,
     },
     lg: {
+      gap: "3.125rem",
+      padding: "5rem 1rem",
       maxWidth: WIDTH_LG,
     },
     md: {
       maxWidth: WIDTH_MD,
-    },
-    sm: {
-      padding: "5rem 1rem",
     },
   }),
 ]);
@@ -41,23 +40,40 @@ export const wrapper = style([
   f.directionColumn,
   {
     gap: "1.125rem",
-    padding: "2.5rem",
+    padding: "1rem",
     backgroundColor: "rgba(255, 255, 255, 0.3)",
     boxShadow: "0px 0px 4px 0px rgba(0, 0, 0, 0.16)",
   },
+  responsiveStyle({
+    lg: {
+      padding: "2.5rem",
+    },
+  }),
 ]);
 
 export const title = style([
   {
     fontWeight: 700,
+    fontSize: "16px",
     letterSpacing: "-0.03rem",
     color: "#222222",
   },
+  responsiveStyle({
+    lg: {
+      fontSize: "32px",
+    },
+  }),
 ]);
 
 export const contents = style([
   {
     fontWeight: 300,
+    fontSize: "14px",
     letterSpacing: "-0.04rem",
   },
+  responsiveStyle({
+    lg: {
+      fontSize: "24px",
+    },
+  }),
 ]);
