5f24dc35fc94cb0882836e6c100aa87b26feb271
byeongminn <sekn1997@naver.com>
2025-03-06 13:08:39 +0900

    test: Vercel 배포 테스트

--- src/app/(portfolio)/page.tsx
+++ src/app/(portfolio)/page.tsx
@@ -1,19 +1,18 @@
-// import { About } from "@/features/about/components/About";
-// import { Closing } from "@/features/closing/components/Closing";
-// import { Cover } from "@/features/cover/components/Cover";
-// import { Projects } from "@/features/projects/components/Projects";
-// import { Suspense } from "react";
+import { About } from "@/features/about/components/About";
+import { Closing } from "@/features/closing/components/Closing";
+import { Cover } from "@/features/cover/components/Cover";
+import { Projects } from "@/features/projects/components/Projects";
+import { Suspense } from "react";
 
 export default function Home() {
   return (
     <main>
-      hello world
-      {/* <Suspense fallback={<></>}>
+      <Suspense fallback={<></>}>
         <Cover />
         <About />
         <Projects />
         <Closing />
-      </Suspense> */}
+      </Suspense>
     </main>
   );
 }
